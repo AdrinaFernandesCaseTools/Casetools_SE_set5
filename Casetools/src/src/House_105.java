b1b9cb4a3ede149e4c0a2c10def9adc496c11c6c
exam <exam@Bcalab4-m05>
2024-04-27 09:26:34 +0530

    commit

---
+++ Casetools/src/src/House_105.java
@@ -0,0 +1,16 @@
+package src;
+
+public class House_105 {
+	String color;
+	String Owner;
+	House_105(String color,String Owner)
+	{
+		this.color=color;
+		this. Owner =Owner;
+	}
+	String getOwner()
+	{
+		return Owner;
+	}
+
+}
